e4d921ba94688555321084075a081f8ddcc92c22
Muhriddin <msayfuddinov7@gmail.com>
2025-04-05 15:12:29 +0500

    edit new

--- js/data.js
+++ js/data.js
@@ -1,7 +1,47 @@
 export default [
+  // {
+  //   title: "",
+  //   github: "",
+  //   vercel: "",
+  // },
   {
-    title: "Example",
-    github: "example.uz",
-    vercel: "example.uz",
+    title: "Three Column Preview Card",
+    github: "https://github.com/Mr-Muhridd1n/Three-column-preview-card",
+    vercel: "https://stats-preview-1ztw.vercel.app/",
+  },
+  {
+    title: "Social Links",
+    github: "https://github.com/Mr-Muhridd1n/Social-links",
+    vercel: "https://blog-preview-card-ht87.vercel.app/",
+  },
+  {
+    title: "Blog Preview Card",
+    github: "https://github.com/Mr-Muhridd1n/Blog-preview-card",
+    vercel: "https://blog-preview-card-phi-nine.vercel.app/",
+  },
+  {
+    title: "Cofe Website",
+    github: "https://github.com/Mr-Muhridd1n/cofe-website",
+    vercel: "https://cofe-website-swart.vercel.app/",
+  },
+  {
+    title: "Recipe Page",
+    github: "https://github.com/Mr-Muhridd1n/Recipe-page",
+    vercel: "https://recipe-page-nu-lovat.vercel.app/",
+  },
+  {
+    title: "Sport Website",
+    github: "https://github.com/Mr-Muhridd1n/sport-website",
+    vercel: "https://sport-website-coral.vercel.app/",
+  },
+  {
+    title: "Akademnashr",
+    github: "https://github.com/Mr-Muhridd1n/akademnashr-web-site",
+    vercel: "https://akademnashr-web-site.vercel.app/",
+  },
+  {
+    title: "Ansormed",
+    github: "https://github.com/Mr-Muhridd1n/Ansormed",
+    vercel: "https://ansormed-beryl.vercel.app/",
   },
 ];
